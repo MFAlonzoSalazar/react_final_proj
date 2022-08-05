8bccd3bfd01e29fc29f3e66a47861060a43ae71d
unknown <mfalonzosalazar@gmail.com>
2022-08-05 16:25:33 -0700

    fixed not found page

--- src/components/Static/NotFound.js
+++ src/components/Static/NotFound.js
@@ -1,7 +1,14 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faQuestion} from '@fortawesome/free-solid-svg-icons'
+
 export default function NotFound() {
     return(
-        <div>
-            not found
+        <div style={{paddingTop:"8%"}} >
+            <FontAwesomeIcon icon={faQuestion} size="10x" />
+
+            <h1 style={{paddingTop: "20px"}}>
+                The page you are looking for does not exist, try again
+            </h1> 
         </div>
     )
 }
